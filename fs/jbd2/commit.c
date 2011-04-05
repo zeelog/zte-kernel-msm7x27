dcc967b5077fff704813b9bacc42ae6bfd8300d4
Zhang Huan <zhhuan@gmail.com>
2011-04-05 19:16:20 -0400

    jbd2: fix potential memory leak on transaction commit
    There is potential memory leak of journal head in function
    jbd2_journal_commit_transaction. The problem is that JBD2 will not
    reclaim the journal head of commit record if error occurs or journal
    is abotred.
    
    I use the following script to reproduce this issue, on a RHEL6
    system. I found it very easy to reproduce with async commit enabled.
    
    mount /dev/sdb /mnt -o journal_checksum,journal_async_commit
    touch /mnt/xxx
    echo offline > /sys/block/sdb/device/state
    sync
    umount /mnt
    rmmod ext4
    rmmod jbd2
    
    Removal of the jbd2 module will make slab complaining that
    "cache `jbd2_journal_head': can't free all objects".
    
    Signed-off-by: Zhang Huan <zhhuan@gmail.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/jbd2/commit.c
+++ fs/jbd2/commit.c
@@ -104,6 +104,8 @@ static int journal_submit_commit_record(journal_t *journal,
 	int barrier_done = 0;
 	struct timespec now = current_kernel_time();
 
+	*cbh = NULL;
+
 	if (is_journal_aborted(journal))
 		return 0;
 
@@ -851,7 +853,7 @@ wait_for_iobuf:
 		if (err)
 			__jbd2_journal_abort_hard(journal);
 	}
-	if (!err && !is_journal_aborted(journal))
+	if (cbh)
 		err = journal_wait_on_commit_record(journal, cbh);
 
 	if (err)
