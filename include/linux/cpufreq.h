d8ffc2b58619047e0b4bf32eabec51fb0bf4624f
zeelog <daniel.zammit@live.com>
2014-01-19 23:48:52 +0100

    enable savagedzen & interactivex governors
    Change-Id: I4ac313d3d489790c37a3ba1b5e26bd37845f8470

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -381,6 +381,12 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lagfree)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAVAGEDZEN)
+extern struct cpufreq_governor cpufreq_gov_savagedzen;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_savagedzen)
 #endif
 
 
